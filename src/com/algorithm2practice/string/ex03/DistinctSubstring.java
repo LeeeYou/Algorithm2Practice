0cb4dd18c990794f8fe1df166df1ea496f549e88
Leee You <leeeyou@163.com>
2016-03-14 22:30:48 +0800

    最长无重复字符子串练习题

---
+++ src/com/algorithm2practice/string/ex03/DistinctSubstring.java
@@ -0,0 +1,19 @@
+package com.algorithm2practice.string.ex03;
+
+/**
+ * Created by LeeeYou on 2016/3/14. 最长无重复字符子串练习题
+ * <p>
+ * 对于一个字符串,请设计一个高效算法，找到字符串的最长无重复字符的子串长度。
+ * 给定一个字符串A及它的长度n，请返回它的最长无重复字符子串长度。保证A中字符全部为小写英文字符，且长度小于等于500。
+ * 测试样例：
+ * "aabcb",5
+ * 返回：3
+ */
+public class DistinctSubstring {
+
+    public int longestSubstring(String A, int n) {
+        // write code here
+        return 1;
+    }
+
+}
