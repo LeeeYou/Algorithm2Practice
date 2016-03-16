d27d01f3e23df3d8fd421671b8f684d4b40d1993
LeeeYou <leeeyou@163.com>
2016-03-16 11:54:38 +0800

    最长无重复字符子串练习题

--- src/com/algorithm2practice/string/ex03/DistinctSubstring.java
+++ src/com/algorithm2practice/string/ex03/DistinctSubstring.java
@@ -1,5 +1,8 @@
 package com.algorithm2practice.string.ex03;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by LeeeYou on 2016/3/14. 最长无重复字符子串练习题
  * <p>
@@ -11,9 +14,34 @@ package com.algorithm2practice.string.ex03;
  */
 public class DistinctSubstring {
 
-    public int longestSubstring(String A, int n) {
-        // write code here
-        return 1;
+    public static void main(String[] args) {
+        System.out.println(longestSubstring("spnvw", 5));
+        System.out.println(longestSubstring("rfrxkfrb", 8));
+    }
+
+    public static int longestSubstring(String A, int n) {
+        if (A == null || A.equals(" ") || n <= 0) {
+            return 0;
+        }
+
+        char[] chars = A.toCharArray();
+        int[] map = new int[256];//保存字符上次出现的位置
+        for (int i = 0; i < 256; i++) {
+            map[i] = -1;
+        }
+
+        int len = 0;
+        int pre = -1;//记录 chars[i-1] 位置的字符上次重复出现的位置
+        int cur = 0;//记录当前最长无重复字符串长度
+
+        for (int i = 0; i < chars.length; i++) {
+            pre = Math.max(pre, map[chars[i]]);
+            cur = i - pre;
+            len = Math.max(len, cur);
+            map[chars[i]] = i;
+        }
+
+        return len;
     }
 
 }
